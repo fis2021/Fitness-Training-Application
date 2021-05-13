c375d663e5a0f8df0b3b25dda3ed421be1ffa85c
Iuga Stefania <iugastefania26@gmail.com>
2021-05-13 19:06:46 +0300

    FTA-36 Apply to a zoom meeting

--- src/main/java/org/fta/Services/ChooseService.java
+++ src/main/java/org/fta/Services/ChooseService.java
@@ -11,4 +11,24 @@ import org.fta.Models.ChooseModel;
 import org.fta.Models.FitnessProgramModel;
 
 public class ChooseService {
+
+    private static ObjectRepository<ChooseModel> ChooseProgramRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("chosen-programs.db").toFile())
+                .openOrCreate("test", "test");
+
+        ChooseProgramRepository = database.getRepository(ChooseModel.class);
+    }
+
+    public static void addChosenProgram(String trainer,String exercise)
+    {
+        ChooseProgramRepository.insert(new ChooseModel(trainer,exercise));
+    }
+
+    public static void removeChosen()
+    {
+        ChooseProgramRepository.remove(ObjectFilters.ALL);
+    }
 }
