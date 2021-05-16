425269c0e72d01dcba54bc9981dd1fd3f855fe1f
Margarit Maria <tia.margarit@gmail.com>
2021-05-16 18:13:16 +0300

    FTA-49 Create Junit test class for ClientService

--- src/main/java/org/fta/Services/FileSystemService.java
+++ src/main/java/org/fta/Services/FileSystemService.java
@@ -1,14 +1,24 @@
 package org.fta.Services;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileSystemService {
-    private static final String APPLICATION_FOLDER = ".fis-fta";
+
+    public static String APPLICATION_FOLDER = ".fis-fta";
     private static final String USER_FOLDER = System.getProperty("user.home");
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
     public static Path getPathToFile(String... path) {
-        return APPLICATION_HOME_PATH.resolve(Paths.get(".", path));
+        return getApplicationHomeFolder().resolve(Paths.get(".", path));
+    }
+
+    public static Path getApplicationHomeFolder(){ return Paths.get(USER_FOLDER, APPLICATION_FOLDER);}
+
+    public static void initDirectory(){
+        Path applicationHomePath = getApplicationHomeFolder();
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
     }
 }
--- src/test/java/org/fta/Services/ClientServiceTest.java
+++ src/test/java/org/fta/Services/ClientServiceTest.java
@@ -1,7 +1,75 @@
 package org.fta.Services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.apache.commons.io.FileUtils;
+import org.fta.Exceptions.UsernameAlreadyExistsException;
+import org.fta.Models.ClientModel;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.testfx.assertions.api.Assertions.assertThat;
 
 class ClientServiceTest {
+
+    public static final String ADMIN = "admin";
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After each");
+        ClientService.getDatabase().close();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before Class");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After Class");
+    }
+
+    @BeforeEach
+    void setup() throws Exception{
+        FileSystemService.APPLICATION_FOLDER=".test-fta";
+        FileSystemService.initDirectory();
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
+        ClientService.initDatabase();
+        System.out.println("Before each");
+    }
+
+    @Test
+    @DisplayName("Database is initialized, and there are no users")
+    void testDatabaseIsInitializedAndNoUserIsPersisted(){
+        assertThat(ClientService.getAllUsers()).isNotNull();
+        assertThat(ClientService.getAllUsers()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("User is successfully persisted to Database")
+    void testUserIsAddedToDatabase() throws UsernameAlreadyExistsException{
+        ClientService.addUser(ADMIN,ADMIN,ADMIN);
+        assertThat(ClientService.getAllUsers()).isNotEmpty();
+        assertThat(ClientService.getAllUsers()).size().isEqualTo(1);
+        ClientModel user = ClientService.getAllUsers().get(0);
+        assertThat(user).isNotNull();
+        assertThat(user.getUsername()).isEqualTo(ADMIN);
+        assertThat(user.getPassword()).isEqualTo(ClientService.encodePassword(ADMIN, ADMIN));
+        assertThat(user.getRole()).isEqualTo(ADMIN);
+    }
+
+    @Test
+    @DisplayName("User can not be added twice")
+    void testUserCanNotBeAddedTwice(){
+        assertThrows(UsernameAlreadyExistsException.class, () -> {
+            ClientService.addUser(ADMIN, ADMIN, ADMIN);
+            ClientService.addUser(ADMIN, ADMIN, ADMIN);
+        });
+    }
+
 
 }
